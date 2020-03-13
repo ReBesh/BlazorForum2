e46e0f4ce39aa4fd99827ef3320fd6129f61c9c1
xhovana22 <xgjinaj16@epoka.edu.al>
2020-03-13 19:14:37 +0100

    shtimet e 3 modele...

---
+++ BlazorApp1/Models/SitePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Models
+{
+    public class SitePage
+    {
+        public int SitePageId { get; set; }
+
+        [MaxLength(60)]
+        [Required]
+        public string Title { get; set; }
+
+        public string MainContent { get; set; }
+
+        public bool AllowDelete { get; set; }
+
+        public bool IsIndex { get; set; }
+    }
+}
